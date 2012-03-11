bfc7726a2781d0c4e5389b22b852ecc51a622937
CPSC 473 <cpsc473@cpsc473.(none)>
2012-03-10 21:18:42 -0800

    Done with exercise 5.5.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
 
    def title
-   base_title = "Ruby on Rails Tutorial Sample App"
-   if @title.nil?
-      base_title
-   else
-      "#{base_title} | #{@title}"
-    end
+     base_title = "Ruby on Rails Tutorial Sample App"
+     if @title.nil?
+       base_title
+     else
+       "#{base_title} | #{@title}"
+     end
+   end
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
 end
